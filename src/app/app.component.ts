6ce3e5142ecb6970b45523b9bc4672adf88527f7
Artur <apaj04@gmail.com>
2023-07-11 13:18:40 +0200

    Finished application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-game';
+  generatedSentence: string = lorem.sentence();
+  typingText: string = '';
+
+  onInput(event: any) {
+    this.typingText = event.target.value;
+  }
+
+  compare(letter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return letter == enteredLetter ? 'correct' : 'incorrect';
+
+  }
 }
